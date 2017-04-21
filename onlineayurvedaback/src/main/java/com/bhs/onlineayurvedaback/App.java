3b4341f0510ae4cc190143fe18a0e0be7483659d
Bhavesh Sonawane <bsonawane118@gmail.com>
2017-04-22 01:00:00 +0530

    My Repository Commited Successfully.

---
+++ onlineayurvedaback/src/main/java/com/bhs/onlineayurvedaback/App.java
@@ -0,0 +1,13 @@
+package com.bhs.onlineayurvedaback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
