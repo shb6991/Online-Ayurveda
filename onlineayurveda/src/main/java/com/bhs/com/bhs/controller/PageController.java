aa48c67b695724c993c718f15968c82764300e0d
Bhavesh Sonawane <bsonawane118@gmail.com>
2017-05-03 20:49:37 +0530

    this is the second commit.

---
+++ onlineayurveda/src/main/java/com/bhs/com/bhs/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.bhs.com.bhs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("greeting","welcome");
+		return mv;
+	}
+}
