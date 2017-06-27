0380a66f2bd4dd76df89eb8fbf534a28811c50ad
Bhavesh Sonawane <bsonawane118@gmail.com>
2017-06-27 23:01:06 +0530

    5-5

--- onlineayurveda/src/main/java/com/bhs/com/bhs/controller/ManagementController.java
+++ onlineayurveda/src/main/java/com/bhs/com/bhs/controller/ManagementController.java
@@ -2,6 +2,7 @@ package com.bhs.com.bhs.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bhs.com.bhs.util.FileUploadUtility;
 import com.bhs.onlineayurvedaback.dao.CategoryDAO;
 import com.bhs.onlineayurvedaback.dao.ProductDAO;
 import com.bhs.onlineayurvedaback.dto.Category;
@@ -63,7 +65,7 @@ public class ManagementController {
 	//handling product submission
 	
 	@RequestMapping(value = "/products", method =RequestMethod.POST) 
-	public String handleProductSubmission(@Valid @ModelAttribute("product") Product mProduct, BindingResult results, Model model){
+	public String handleProductSubmission(@Valid @ModelAttribute("product") Product mProduct, BindingResult results, Model model, HttpServletRequest request){
 		
 		//check if any errors
 		
@@ -84,6 +86,14 @@ public class ManagementController {
 		//creating new product
 		
 		productDAO.add(mProduct);
+		
+		if(!mProduct.getFile().getOriginalFilename().equals("")){
+			
+			FileUploadUtility.uploadFile(request, mProduct.getFile(), mProduct.getCode());
+			
+		}
+		
+		
 		
 						
 		return "redirect:/manage/products?operation = product";
--- onlineayurveda/src/main/webapp/WEB-INF/views/manageProducts.jsp
+++ onlineayurveda/src/main/webapp/WEB-INF/views/manageProducts.jsp
@@ -35,6 +35,7 @@
 					<sf:form class="form-horizontal" modelAttribute = "product"
 					action = "${contextRoot }/manage/products"
 					method = "POST"
+					enctype = "multipart/form-data"
 					>
 
 						<div class="form-group">
@@ -109,6 +110,22 @@
 							</div>
 
 						</div>
+						
+						<!-- File element for image upload -->
+						
+						<div class="form-group">
+
+							<label class="control-lable col-md-4" for="file">Select An Image :</label>
+
+							<div class="col-md-8">
+
+								<sf:input type="file" path="file" id="file"
+									 class="form-control" />
+
+							</div>
+
+						</div>
+						
 
 						<div class="form-group">
 
